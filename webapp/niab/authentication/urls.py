1403588a925e78d4fc38e17a015fc6f3e5e0b432
Quentin <jakimpact@gmail.com>
2024-04-04 16:01:54 +0200

    PBO-13 Création infrastructure tailwind avec django

---
+++ webapp/niab/authentication/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from authentication import views
+
+app_name = 'authentication'
+
+urlpatterns = [
+    
+]
--- webapp/niab/niab/urls.py
+++ webapp/niab/niab/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authentication/', include('authentication.urls')),
-    path('dashboard/', include('dashboard.urls')),
+    path('', include('dashboard.urls')),
 ]
