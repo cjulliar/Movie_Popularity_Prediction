0926239009d38e2a4805c26ebbe7d65b1338d8af
SimonRqrt <simon.riquart@icloud.com>
2024-04-09 15:41:08 +0200

    moviesdata extrait

--- scrapper/scrapper/spiders/jpbox2.py
+++ scrapper/scrapper/spiders/jpbox2.py
@@ -9,7 +9,7 @@ class JpspiderSpider(scrapy.Spider):
     urls_vues = set()
     custom_settings = {
     'FEEDS' : {
-        'jpboxoffice.json' : {'format' : 'json', 'overwrite' : True},
+        'jpboxoffice.csv' : {'format' : 'csv', 'overwrite' : True},
         }
     }
 
@@ -17,19 +17,20 @@ class JpspiderSpider(scrapy.Spider):
         self.logger.info("Analyse de la page principale du film : %s", response.url)
         films = response.xpath('/html/body/div[5]/table[2]')
         url_socle = "https://www.jpbox-office.com/"
-        first_week_entries = response.css('table.tablesmall.tablesmall5 tr td.col_poster_contenu_majeur::text').get()
-        copies = response.css('table.tablesmall.tablesmall5 tr:nth-child(2) td:nth-child(7)::text').get()
+       
         for film in films:
-            film_url = film.xpath('//h3/a/@href').getall()
+            film_url = film.xpath('.//h3/a/@href').getall()
+
             if film_url:
                 for url in film_url:
                     full_url = url_socle + url
+
                     if full_url in self.urls_vues:
                         continue
                     else:
                         self.urls_vues.add(full_url)
-                    yield scrapy.Request(full_url, callback=self.parse_movie,meta={'first_week_entries' : first_week_entries, 'copies' : copies})
-            else:
+
+                    yield scrapy.Request(full_url, callback=self.parse_movie)
                 self.logger.warning("Aucune URL de film trouvée dans la ligne : %s", film.extract())
 
         current_page = response.meta.get('current_page', 0)
@@ -41,8 +42,6 @@ class JpspiderSpider(scrapy.Spider):
 
     def parse_movie(self, response):
         item = JpboxItem()
-        first_week_entries = response.meta.get('first_week_entries')
-        copies = response.meta.get('copies')
 
         item['url'] = response.url
         item['title'] = response.xpath('//h1/text()').get()
@@ -51,9 +50,7 @@ class JpspiderSpider(scrapy.Spider):
         item['genre'] = response.css('table.table_2022titre h3 a:nth-of-type(2)::text').get()
         item['date'] = response.xpath('//table[@class="tablelarge1"]//div//p//a/text()').get()
         item['studio'] = response.xpath('//h3[text()="Distribué par"]/following-sibling::text()[1]')[-1].get()
-        item['first_week_entries'] = first_week_entries
-        item['copies'] = copies
-        item['casting'] = response.xpath('//div[5]/div[1]/ul/li[6]/a/text()')[1].extract().strip()
+        item['first_week_entries'] = response.css('table.tablesmall.tablesmall2 tr:last-child  td.col_poster_contenu_majeur::text').get()
         item['franchise'] = response.xpath('//div[@id="nav2"]//ul//a[contains(text(), "Franchise")]/text()').get()
         item['remake'] = response.xpath('//div[@id="nav2"]//ul//a[contains(text(), "Remake")]/text()').get()
         
@@ -66,20 +63,13 @@ class JpspiderSpider(scrapy.Spider):
             casting_url = response.xpath('//*[@id="nav2"]/ul/li[6]/a/@href').get()
         else:
             casting_url = None
+
         if casting_url:
             yield response.follow(casting_url, callback=self.parse_casting, meta={'item': item})
 
         budget_url = response.xpath('//*[@id="nav2"]/ul/li[1]/a/@href').get()
         yield response.follow(budget_url, callback=self.parse_budget, meta={'item' : item})
 
-        if casting_url:
-            request = scrapy.Request(response.urljoin(casting_url), callback=self.parse_casting, meta={'item': item})
-            request.meta['budget_url'] = budget_url
-            yield request
-        elif budget_url:  # Si la date de sortie n'est pas nécessaire ou absente
-            yield scrapy.Request(response.urljoin(budget_url), callback=self.parse_budget, meta={'item': item})
-        else:
-            yield item
 
     def parse_casting(self, response):
         self.logger.info("Analyse de la page du casting: %s", response.url)
